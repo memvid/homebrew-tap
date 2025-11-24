41c0fef3881c0d7091d72efc7eeac080a7508797
Mr Sharafdin <83120892+sharafdin@users.noreply.github.com>
2025-11-24 15:00:17 +0300

    Update memorycli.rb for version 0.1.2 releases

--- Formula/memorycli.rb
+++ Formula/memorycli.rb
@@ -6,25 +6,25 @@ class Memorycli < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      # memorycli-aarch64-apple-darwin.tar.gz
-      url "https://github.com/memvid/memory-cli/releases/download/v0.1.2/memorycli-x86_64-apple-darwin.tar.gz"
-      sha256 "e59e11b06d7b7e4759028f8b6f6d174a62a611e3f358af2a809406c3260a597e"
+      # ARM macOS (Apple Silicon)
+      url "https://github.com/memvid/memory-cli/releases/download/v0.1.2/memorycli-aarch64-apple-darwin.tar.gz"
+      sha256 "sha256:c3ebeee917028d60b5260551e48fdd425710b4c9b0b7f1246fda26ec76a0cf7b"
     else
-      # memorycli-x86_64-apple-darwin.tar.gz
-      url "https://github.com/memvid/memory-cli/releases/download/v0.1.1/memorycli-x86_64-apple-darwin.tar.gz"
-      sha256 "PASTE_FULL_SHA256_FOR_x86_64_apple_darwin_HERE"
+      # Intel macOS
+      url "https://github.com/memvid/memory-cli/releases/download/v0.1.2/memorycli-x86_64-apple-darwin.tar.gz"
+      sha256 "sha256:e59e11b06d7b7e4759028f8b6f6d174a62a611e3f358af2a809406c3260a597e"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      # memorycli-aarch64-unknown-linux-gnu.tar.gz
-      url "https://github.com/memvid/memory-cli/releases/download/v0.1.1/memorycli-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "PASTE_FULL_SHA256_FOR_aarch64_unknown_linux_gnu_HERE"
+      # ARM Linux
+      url "https://github.com/memvid/memory-cli/releases/download/v0.1.2/memorycli-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "sha256:4cc5bfe27dfdffa2c94ec5b7174f4ad0586ea85a63604653f3931b8ca299d1c4"
     else
-      # memorycli-x86_64-unknown-linux-gnu.tar.gz
-      url "https://github.com/memvid/memory-cli/releases/download/v0.1.1/memorycli-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "PASTE_FULL_SHA256_FOR_x86_64_unknown_linux_gnu_HERE"
+      # Intel Linux
+      url "https://github.com/memvid/memory-cli/releases/download/v0.1.2/memorycli-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "sha256:fe68d0fd78ef8f37f376c42c917cb0b791fc20878ab8dc9ea441718e88fbe441"
     end
   end
 
@@ -33,7 +33,6 @@ class Memorycli < Formula
   end
 
   test do
-    output = shell_output("#{bin}/memorycli")
-    assert_match "Hello", output
+    assert_match "Hello", shell_output("#{bin}/memorycli")
   end
 end
