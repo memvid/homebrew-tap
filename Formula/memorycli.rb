67c5682a021a9e3f80e8b535c6661367b6868e2b
sharafdin <isasharafdin@gmail.com>
2025-11-24 14:20:05 +0300

    initial formula

---
+++ Formula/memorycli.rb
@@ -0,0 +1,34 @@
+class Memorycli < Formula
+  desc "A simple CLI for testing deployment pipelines"
+  homepage "https://github.com/memvid/memory-cli"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/memvid/memory-cli/releases/download/v0.1.0/memorycli-aarch64-apple-darwin.tar.gz"
+      sha256 "REPLACE_WITH_ACTUAL_SHA256"
+    else
+      url "https://github.com/memvid/memory-cli/releases/download/v0.1.0/memorycli-x86_64-apple-darwin.tar.gz"
+      sha256 "REPLACE_WITH_ACTUAL_SHA256"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/memvid/memory-cli/releases/download/v0.1.0/memorycli-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "REPLACE_WITH_ACTUAL_SHA256"
+    else
+      url "https://github.com/memvid/memory-cli/releases/download/v0.1.0/memorycli-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "REPLACE_WITH_ACTUAL_SHA256"
+    end
+  end
+
+  def install
+    bin.install "memorycli"
+  end
+
+  test do
+    assert_match "Hello, World!", shell_output("#{bin}/memorycli")
+  end
+end
