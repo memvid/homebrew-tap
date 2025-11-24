62d9ac74f6e5a096ca76d27613a58f26b6a54fa6
Mr Sharafdin <83120892+sharafdin@users.noreply.github.com>
2025-11-24 14:34:30 +0300

    memorycli 0.1.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/memorycli.rb
+++ Formula/memorycli.rb
@@ -1,13 +1,13 @@
 class Memorycli < Formula
   desc "A simple CLI for testing deployment pipelines"
   homepage "https://github.com/memvid/memory-cli"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/memvid/memory-cli/releases/download/v0.1.0/memorycli-aarch64-apple-darwin.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256"
+      url "https://github.com/memvid/memory-cli/releases/download/v0.1.1/memorycli-x86_64-apple-darwin.tar.gz"
+      sha256 "958a97e33d8d9276c3b992bcfe093690c81d2115be4e36a0ae06d1262b87131d"
     else
       url "https://github.com/memvid/memory-cli/releases/download/v0.1.0/memorycli-x86_64-apple-darwin.tar.gz"
       sha256 "REPLACE_WITH_ACTUAL_SHA256"
